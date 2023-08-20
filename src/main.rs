929937e51f8a767c8024a90216d4668df9296342
leefordjudes <leefordjudes@gmail.com>
2023-08-20 22:34:18 +0530

    3.9-passing-children-to-components

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,70 @@ use leptos::*;
 
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
-    let items = create_rw_signal(cx, Vec::new());
-    items.set(vec![1.to_string(), 2.to_string()]);
-    // let on_click = move |_| {
-    //     let nos = items.get().len() as u32;
-    //     items.update(|x| x.push(nos.to_string()));
-    // };
-    let ul_list = move || {
-        items
-            .get()
-            .into_iter()
-            .map(|x| view! {cx, <li>{x}</li>})
-            .collect_view(cx);
-    };
-    let list = move || {
+    // TakesChildrenFnOnce
+    let msg = "try leptos".to_string();
+    let children_fn_once = move || {
         view! {
             cx,
-            // <button on:click=on_click >"INC"</button>
-            <ul>{ul_list}</ul>
+            <p>{msg}</p>
         }
+        .into_view(cx)
+    };
+    // TakesChildrenFn
+    let numbers = create_rw_signal(cx, vec![1, 2, 3]);
+    let on_click = move |_| {
+        let len = numbers.get_untracked().len();
+        numbers.update(|x| x.push(len + 1));
+    };
+    let number_list = move || {
+        numbers
+            .get()
+            .into_iter()
+            .map(|x| view! {cx,<li>{x}</li>})
+            .collect_view(cx)
+    };
+    // TakesChildrenFnMut
+    let mut sum = 0;
+    let mut total = move || {
+        sum = numbers.with(|x| x.iter().fold(0, |s, y| s + y));
+        view! {cx,<p>{sum}</p>}.into_view(cx)
     };
     view! { cx,
-        <TakesChildren>
-            <ul>{ul_list}</ul>
-        </TakesChildren>
+        <TakesChildrenFnOnce>
+        {children_fn_once()}
+        </TakesChildrenFnOnce>
+        <hr/>
+        <button on:click=on_click>"Inc"</button>
+        <TakesChildrenFn>
+        <ul>{number_list}</ul>
+        </TakesChildrenFn>
+        <hr/>
+        <TakesChildrenFnMut>
+        {total()}
+        </TakesChildrenFnMut>
     }
 }
 
 #[component]
-pub fn TakesChildren(cx: Scope, children: Children) -> impl IntoView {
+pub fn TakesChildrenFnOnce(cx: Scope, children: Children) -> impl IntoView {
     view! { cx,
-        <h2>"Children"</h2>
+        <h2>"Takes Children FnOnce"</h2>
+        {children(cx)}
+    }
+}
+#[component]
+pub fn TakesChildrenFn(cx: Scope, children: ChildrenFn) -> impl IntoView {
+    view! { cx,
+        <h2>"Takes Children Fn"</h2>
+        <ul>
+        {children(cx)}
+        </ul>
+    }
+}
+#[component]
+pub fn TakesChildrenFnMut(cx: Scope, mut children: ChildrenFnMut) -> impl IntoView {
+    view! { cx,
+        <h2>"Takes Children FnMut"</h2>
         {children(cx)}
     }
 }
